d8039810cd38cef64188baccb30c0edd2b633e61
MukkuMayc <st063775@student.spbu.ru>
2020-07-02 02:01:58 +0300

    Add ring communication

---
+++ mpi/ring.cpp
@@ -0,0 +1,37 @@
+#include <mpi.h>
+#include <string>
+#include <iostream>
+
+void print_group_status(int group_size, int group_rank) {
+    printf("GROUP RANK/SIZE: %d/%d\n", group_rank, group_size);
+}
+
+void ring(MPI_Comm group_comm)  {
+    int group_rank, group_size;
+    MPI_Comm_rank(group_comm, &group_rank);
+    MPI_Comm_size(group_comm, &group_size);
+        
+
+    if (group_rank != 0) {
+        char* inpmsg = new char[128];
+        MPI_Status stat;
+        int proc_prev = group_rank - 1;
+        MPI_Recv(inpmsg, 128, MPI_CHAR, proc_prev, 1, group_comm, &stat);
+        print_group_status(group_size, group_rank);
+        std::cout << "Receive message from " << proc_prev << ". Message: " <<
+            inpmsg << std::endl;
+    }
+    else {
+        std::cout << "I'm first process!\n";
+        print_group_status(group_size, group_rank);
+    }
+
+    if (group_rank != group_size - 1) {
+        int dest = group_rank + 1;
+        std::string outmsg = "You're next, process " + std::to_string(dest);
+        MPI_Send(outmsg.data(), outmsg.length(), MPI_CHAR, dest, 1, group_comm);
+    }
+    else {
+        std::cout << "I'm last process!\n";
+    }
+}
--- mpi/task_1.cpp
+++ mpi/task_1.cpp
@@ -1,31 +1,14 @@
 #include <mpi.h>
 #include <stdio.h>
 
+#include "ring.cpp"
+
 int main(int argc, char** argv) {
     // Initialize the MPI environment
     MPI_Init(NULL, NULL);
 
-    // Get the rank and size in the original communicator
-    int world_rank, world_size;
-    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
-
-    int color = world_rank / 3; // Determine color based on row
-
-    // Split the communicator based on the color and use the
-    // original rank for ordering
-    MPI_Comm row_comm;
-    MPI_Comm_split(MPI_COMM_WORLD, color, world_rank, &row_comm);
-
-    int row_rank, row_size;
-    MPI_Comm_rank(row_comm, &row_rank);
-    MPI_Comm_size(row_comm, &row_size);
-
-    printf("WORLD RANK/SIZE: %d/%d \t ROW RANK/SIZE: %d/%d\n",
-    world_rank, world_size, row_rank, row_size);
-
-    MPI_Comm_free(&row_comm);
-
+    ring(MPI_COMM_WORLD);
+    
     // Finalize the MPI environment.
     MPI_Finalize();
 }
